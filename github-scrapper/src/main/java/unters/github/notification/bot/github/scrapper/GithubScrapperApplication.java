7d3f1226e7ab96a1d23fd47ff2cab94b71d68338
Artem Gromakov <gromakov.art3m@gmail.com>
2024-11-10 22:03:04 +0300

    Added project structure

---
+++ github-scrapper/src/main/java/unters/github/notification/bot/github/scrapper/GithubScrapperApplication.java
@@ -0,0 +1,12 @@
+package unters.github.notification.bot.github.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubScrapperApplication.class, args);
+    }
+}
